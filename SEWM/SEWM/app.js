0100e1f8812c35fee19be09b5f02600e3651b0aa
kushal <kushaldeveloper@gmail.com>
2016-04-05 16:33:37 -0400

    add a button click

--- SEWM/SEWM/app.js
+++ SEWM/SEWM/app.js
@@ -33,4 +33,8 @@ window.onload = function () {
     var greeter = new Greeter(el);
     greeter.start();
 };
+function onBtnSubmitClicked() {
+    var car = new Car("V8");
+    car.start();
+}
 //# sourceMappingURL=app.js.map
